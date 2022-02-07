397ff076273f4d15cfbeb2884aee7b0e2153d0c9
S8U <0uh1004@gmail.com>
2022-02-07 17:34:05 +0900

    DisplayName Module 추가 (미완성)

---
+++ src/main/java/net/pooleaf/core/modules/displayname/DisplayNameManager.java
@@ -0,0 +1,15 @@
+package net.pooleaf.core.modules.displayname;
+
+import net.pooleaf.core.modules.support.common.manager.AbstractManager;
+
+import java.util.UUID;
+
+public class DisplayNameManager extends AbstractManager<UUID, String> {
+
+    @Override
+    public String load(UUID key) {
+        // TODO 불러오기 구현
+        return super.load(key);
+    }
+
+}
