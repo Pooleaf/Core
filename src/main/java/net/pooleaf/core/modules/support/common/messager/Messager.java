755c4b3b913b2e1593129dad51639cffeffc8cde
S8U <0uh1004@gmail.com>
2022-02-09 17:34:15 +0900

    Prefix 없는 메시지 메소드들 추가

--- src/main/java/net/pooleaf/core/modules/support/common/messager/Messager.java
+++ src/main/java/net/pooleaf/core/modules/support/common/messager/Messager.java
@@ -9,28 +9,54 @@ public class Messager extends Prefixer {
     private static MessagerAdapter messagerAdapter;
 
 
-    public static void message(Object sender, Object message) {
+    public static void nmessage(Object sender, Object message) {
         messagerAdapter.message(sender, getCurrentPluginPrefix() + " §f" + message);
+    }
+
+    public static void nmessage(Object sender, Object message, Object... params) {
+        nmessage(sender, String.format((String) message, params));
+    }
+
+    public static void message(Object sender, Object message) {
+        nmessage(sender, getCurrentPluginPrefix() + " §f" + message);
     }
 
     public static void message(Object sender, Object message, Object... params) {
         message(sender, String.format((String) message, params));
     }
 
+
+    public static void nwarning(Object sender, Object message) {
+        nmessage(sender, "§c" + message);
+    }
+
+    public static void nwarning(Object sender, Object message, Object... params) {
+        nwarning(sender, String.format((String) message, params));
+    }
+
     public static void warning(Object sender, Object message) {
-        message(sender, " §c" + message);
+        nmessage(sender, getCurrentPluginPrefix() + " §c" + message);
     }
 
     public static void warning(Object sender, Object message, Object... params) {
         warning(sender, String.format((String) message, params));
     }
 
-    public static void broadcast(Object message) {
-        broadcast(getCurrentPluginPrefix() + " §f" + message);
-    }
 
     public static void nbroadcast(Object message) {
-        broadcast(message);
+        messagerAdapter.broadcast(message);
+    }
+
+    public static void nbroadcast(Object message, Object... params) {
+        messagerAdapter.broadcast(String.format((String) message, params));
+    }
+
+    public static void broadcast(Object message) {
+        nbroadcast(getCurrentPluginPrefix() + " §f" + message);
+    }
+
+    public static void broadcast(Object message, Object... params) {
+        broadcast(String.format((String) message, params));
     }
 
 }
