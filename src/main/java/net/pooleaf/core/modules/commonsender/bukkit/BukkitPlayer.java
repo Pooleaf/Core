22ebcddfbe886821b2155f72aaa3bef13a59730a
S8U <0uh1004@gmail.com>
2022-02-15 15:50:59 +0900

    DisplayName 플랫폼에 적용

--- src/main/java/net/pooleaf/core/modules/commonsender/bukkit/BukkitPlayer.java
+++ src/main/java/net/pooleaf/core/modules/commonsender/bukkit/BukkitPlayer.java
@@ -7,6 +7,16 @@ import org.bukkit.entity.Player;
 public class BukkitPlayer extends CommonPlayer<Player> {
 
   @Override
+  public void setDisplayName(String displayName) {
+    super.setDisplayName(displayName);
+
+    // 온라인일 경우 실제 닉네임도 변경
+    if (isOnline()) {
+      getPlatformSender().setDisplayName(displayName);
+    }
+  }
+
+  @Override
   public Player getPlatformSender() {
     return Bukkit.getPlayer(uuid);
   }
--- src/main/java/net/pooleaf/core/modules/commonsender/bungee/BungeePlayer.java
+++ src/main/java/net/pooleaf/core/modules/commonsender/bungee/BungeePlayer.java
@@ -7,6 +7,16 @@ import net.pooleaf.core.modules.commonsender.common.CommonPlayer;
 public class BungeePlayer extends CommonPlayer<ProxiedPlayer> {
 
   @Override
+  public void setDisplayName(String displayName) {
+    super.setDisplayName(displayName);
+
+    // 온라인일 경우 실제 닉네임도 변경
+    if (isOnline()) {
+      getPlatformSender().setDisplayName(displayName);
+    }
+  }
+
+  @Override
   public ProxiedPlayer getPlatformSender() {
     return ProxyServer.getInstance().getPlayer(uuid);
   }
