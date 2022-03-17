3384500e3191d75a4ff2dd0fb7b75f2d08843e07
S8U <0uh1004@gmail.com>
2022-03-17 17:30:09 +0900

    DeleteContext addBatch 메소드 추가

--- src/main/java/net/pooleaf/core/modules/sqllib/common/dslcontext/DeleteContext.java
+++ src/main/java/net/pooleaf/core/modules/sqllib/common/dslcontext/DeleteContext.java
@@ -1,9 +1,23 @@
 package net.pooleaf.core.modules.sqllib.common.dslcontext;
 
+import lombok.Cleanup;
+import lombok.SneakyThrows;
 import net.pooleaf.core.modules.sqllib.common.AbstractSqlManager;
 import net.pooleaf.core.modules.sqllib.common.SqlTable;
+import net.pooleaf.core.modules.support.common.debugger.Debugger;
+
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DeleteContext extends DslContext<DeleteContext> {
+
+    private List<Object> values;
+
+    private List<List<Object>> batches = new ArrayList<>(); // Batch로 Insert문 여러개 사용할 때 값들을 저장해둘 List
+
 
     public DeleteContext(AbstractSqlManager sqlManager, SqlTable sqlTable) {
         super(sqlManager, sqlTable);
@@ -30,7 +44,27 @@ public class DeleteContext extends DslContext<DeleteContext> {
     }
 
     public DeleteContext parameters(Object... parameters) {
-        this.values = parameters;
+        this.values = Arrays.asList(parameters);
+        return this;
+    }
+
+    /**
+     * Batch에 값을 저장하고 현재 값을 초기화합니다.
+     * Batch에 저장된 값은 execute시 함께 저장됩니다.
+     * @return
+     */
+    @SneakyThrows
+    private DeleteContext addBatch() {
+        // 값 설정 안돼있으면 실행 안함
+        if (values == null || values.isEmpty()) {
+            return this;
+        }
+
+        batches.add(values);
+
+        // 값 초기화
+        values = null;
+
         return this;
     }
 
@@ -38,10 +72,41 @@ public class DeleteContext extends DslContext<DeleteContext> {
      * SQL문을 실행하고 삭제 성공한 수를 반환합니다.
      * @return 삭제 성공한 수
      */
-    public int execute() {
-        String sql = buildSql();
+    @SneakyThrows
+    public int[] execute() {
+        long startTime = System.currentTimeMillis();
 
-        int result = sqlManager.update(sql, values);
+        // 설정된 값이 있으면 addBatch
+        if (values != null && !values.isEmpty()) {
+            addBatch();
+        }
+
+        String sql = buildSql();
+        Debugger.log("[SQLib] Delete SQL: " + sql);
+
+        @Cleanup PreparedStatement statement = sqlManager.preparedStatement(sql);
+        for (List<Object> batchValues : batches) {
+            Debugger.log("[SQLib] Delete Parameters: [" + batchValues.stream().map(value -> (String) value).collect(Collectors.joining(", ")) + "]");
+
+            // 값 처리
+            int i = 1;
+            for (Object batchValue : batchValues) {
+                statement.setObject(i, sqlManager.convertValue(batchValue));
+                i++;
+            }
+
+            statement.addBatch();
+            statement.clearParameters();
+        }
+
+        // 실행 후 결과값 출력
+        int[] result = statement.executeBatch();
+        Debugger.log("[SQLib] Insert Result: " + Arrays.stream(result)
+                .mapToObj(r -> String.valueOf(r))
+                .collect(Collectors.joining(", ")));
+
+        Debugger.log("[SQLib] Insert Executed in " + (System.currentTimeMillis() - startTime) + " ms");
+
         return result;
     }
 
