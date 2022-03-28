64f7211cce69d92ac90bba8cf2e73c8711a27b69
S8U <0uh1004@gmail.com>
2022-03-28 11:59:01 +0900

    AbstractPlayer 추가

---
+++ src/main/java/net/pooleaf/core/modules/support/common/player/AbstractPlayer.java
@@ -0,0 +1,31 @@
+package net.pooleaf.core.modules.support.common.player;
+
+import lombok.Data;
+import net.pooleaf.core.modules.commonsender.CommonSenderModule;
+import net.pooleaf.core.modules.commonsender.common.CommonPlayer;
+
+import java.util.UUID;
+
+@Data
+public class AbstractPlayer<T> {
+
+    private final UUID uuid;
+
+
+    public CommonPlayer<T> getCommonPlayer() {
+        return CommonSenderModule.getPlayer(uuid);
+    }
+
+    public String getName() {
+        return getCommonPlayer().getName();
+    }
+
+    public String getDisplayName() {
+        return getCommonPlayer().getDisplayName();
+    }
+
+    public boolean isOnline() {
+        return getCommonPlayer().isOnline();
+    }
+
+}
