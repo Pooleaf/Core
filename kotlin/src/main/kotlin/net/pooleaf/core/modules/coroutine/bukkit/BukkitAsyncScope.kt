a12910cdafe8ff056a8f257f2282906975599f46
0uh1004 <0uh1004@gmail.com>
2023-02-06 20:10:52 +0900

    0.0.28 1. BukkitAsyncScope object로 변경

--- kotlin/src/main/kotlin/net/pooleaf/core/modules/coroutine/bukkit/BukkitAsyncScope.kt
+++ kotlin/src/main/kotlin/net/pooleaf/core/modules/coroutine/bukkit/BukkitAsyncScope.kt
@@ -3,7 +3,7 @@ package net.pooleaf.core.modules.coroutine.bukkit
 import kotlinx.coroutines.CoroutineScope
 import kotlin.coroutines.CoroutineContext
 
-class BukkitAsyncScope: CoroutineScope {
+object BukkitAsyncScope: CoroutineScope {
 
     override val coroutineContext: CoroutineContext = BukkitAsyncDispatcher
 
