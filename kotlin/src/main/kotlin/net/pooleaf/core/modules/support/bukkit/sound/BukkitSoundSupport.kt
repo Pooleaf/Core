578dd06cbda37d2d3ce9ca0cfebc16cc8586fd09
0uh1004 <0uh1004@gmail.com>
2023-03-05 11:16:24 +0900

    1.49.0 BukkitSoundSupport: XSound에 location 파라미터 추가

--- kotlin/src/main/kotlin/net/pooleaf/core/modules/support/bukkit/sound/BukkitSoundSupport.kt
+++ kotlin/src/main/kotlin/net/pooleaf/core/modules/support/bukkit/sound/BukkitSoundSupport.kt
@@ -1,6 +1,7 @@
 package net.pooleaf.core.modules.support.bukkit.sound
 
 import com.cryptomorin.xseries.XSound
+import org.bukkit.Location
 import org.bukkit.entity.Player
 
 fun Player.playSound(
@@ -17,4 +18,31 @@ fun Player.playSoundSafely(
     pitch: Float = 1.0F
 ) {
     if (this.isOnline) sound.play(this, volume, pitch)
+}
+
+fun Player.playSound(
+    sound: XSound,
+    location: Location,
+    volume: Float = 1.0F,
+    pitch: Float = 1.0F
+) {
+    sound.play(this, location, volume, pitch)
+}
+
+fun Player.playSoundSafely(
+    sound: XSound,
+    location: Location,
+    volume: Float = 1.0F,
+    pitch: Float = 1.0F
+) {
+    if (this.isOnline) sound.play(this, location, volume, pitch)
+}
+
+fun XSound.play(
+    player: Player,
+    location: Location,
+    volume: Float = 1.0F,
+    pitch: Float = 1.0F
+) {
+    player.playSound(location, this.parseSound(), volume, pitch)
 }
